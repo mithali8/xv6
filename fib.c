78cc52982b0ed55304079c769df95fcaf98f16aa
mithali8 <43978573+mithali8@users.noreply.github.com>
2019-09-18 08:43:12 +0530

    Add files via upload

---
+++ fib.c
@@ -0,0 +1,18 @@
+//#include"types.h"
+//#include"user.h"
+//#include"stat.h"
+#incude<stdio.h>
+
+int main()
+{
+	int i, num, j;
+	printf("Enter a no:\n");
+	scanf("%d", &num);
+
+	for(j=1;j<n;j++)
+	{
+		i = i*j;
+	}
+	printf("the factorial of %d is %d\n", num, j);
+
+}
